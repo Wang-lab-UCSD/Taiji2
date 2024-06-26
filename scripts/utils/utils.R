5dc35f5fcb29dcbccc4e57d58d6bf22a98613def
cong-003 <56328920+cong-003@users.noreply.github.com>
2024-06-26 10:29:23 -0700

    Update utils.R

--- scripts/utils/utils.R
+++ scripts/utils/utils.R
@@ -4,3 +4,24 @@ firstup <- function(x) {
   x
 }
 
+# create new sub-dir and set the new sub-dir as the working dir
+createDir <- function(subdir){
+    dir.create(file.path(maindir, subdir), showWarnings = FALSE)
+    setwd(file.path(maindir, subdir))
+    print(getwd())
+}
+
+# normalize data across rows to make sure the values is in range [0,1] after norm
+range01 <- function(x){(x-min(x))/(max(x)-min(x))}
+scaleData <- function(df){
+  return(as.data.frame(t(apply(as.matrix(df), MARGIN = 1, FUN = range01))))
+}
+                               
+# get z-scores of data 
+zscore <- function(df){
+  return(as.data.frame(t(scale(t(as.matrix(df))))))
+}                      
+                           
+## calculate covariance, x is the matrix
+co.var <- function(x) { matrixStats::rowSds(x) / matrixStats::rowMeans2(x) }
+
